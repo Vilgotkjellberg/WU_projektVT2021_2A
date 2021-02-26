b9aabfc50efb89cf438cf155ff1ce0b5f290b0be
kjell03 <77670877+kjell03@users.noreply.github.com>
2021-02-26 16:07:52 +0100

    Lagt in alla html docs och små ändrat

--- js/main.js
+++ js/main.js
@@ -15,3 +15,17 @@ function show(){
 function close(){
     mainMenu.style.top = '-100%';
 }
+
+function initMap() {
+    var location = {lat: 57.70, lng: 11.975};
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 4,
+        center: location
+    });
+    var marker = new google.maps.Marker({
+        position: location, 
+        map: map
+    });
+}
+
+
